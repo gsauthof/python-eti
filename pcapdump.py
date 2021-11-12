f2d03f7e9a23965da23654ca7917c7c742658997
Georg Sauthoff <mail@gms.tf>
2021-11-12 17:16:11 +0100

    dump src/dst MAC/IP address/port, add version/count/offset options

--- pcapdump.py
+++ pcapdump.py
@@ -10,13 +10,18 @@
 import argparse
 import dpkt
 from enum import IntEnum
+import importlib
+import socket
 import struct
 import sys
 
-import eobi.v9_1 as eobi
-import eti.v9_1 as eti
+# NB: these are dynamically imported, see below
+#import eobi.v9_1 as eobi
+#import eti.v9_1 as eti
 
-from dressup import pformat
+# NB: dressup is too slow
+#from dressup import pformat
+from pprint import pformat
 
 
 class TLDR(IntEnum):
@@ -35,7 +40,13 @@ tldr2str_map = {
 }
 
 
-def dump_eobi(bs, tos, dump_heartbeat):
+def addr2str(a):
+    return socket.inet_ntop(socket.AF_INET, a)
+
+def m2str(a):
+    return ':'.join(f'{x:02x}' for x in a)
+
+def dump_eobi(bs, tos, dump_heartbeat, eth, ip, udp):
         n = len(bs)
 
         ph = eobi.unpack_from(bs)
@@ -109,18 +120,35 @@ def parse_args():
     p = argparse.ArgumentParser(description='Dump ETI/EOBI messages from PCAP files')
     p.add_argument('filename', help='PCAP file')
     p.add_argument('--love', '-l', action='store_true', help='also dump EOBI heartbeat packets')
+    p.add_argument('--off', '-o', type=int, help='jump to offset after reading pcap header')
+    p.add_argument('--count', '-c', type=int, help='stop after N frames')
+    p.add_argument('--release', '-r', default='9.1', help='parse eti/eobi v9.0 (default: %(default)s)')
     args = p.parse_args()
     return args
 
 def main():
     args = parse_args()
 
+    if args.release:
+        global eti
+        global eobi
+        rel = args.release.replace('.', '_')
+        eobi = importlib.import_module(f'eobi.v{rel}')
+        eti  = importlib.import_module(f'eti.v{rel}')
+
     with open(args.filename, 'rb') as f:
         pcap = dpkt.pcap.Reader(f)
 
-        for ts, buf in pcap:
-            print(f'Captured @ {ts}')
+        if args.off:
+            f.seek(args.off)
+
+        for i, (ts, buf) in enumerate(pcap):
+            if args.count and args.count == i:
+                break
+
             eth = dpkt.ethernet.Ethernet(buf)
+            print(f'Captured @ {ts}, {m2str(eth.src)} -> {m2str(eth.dst)}', end='')
+
             ip  = eth.data
 
             if not isinstance(ip, dpkt.ip.IP):
@@ -132,16 +160,19 @@ def main():
                 udp     = tp
                 payload = memoryview(udp.data)
                 try:
-                    dump_eobi(payload, ip.tos, args.love)
+                    print(f', {addr2str(ip.src)}:{udp.sport} -> {addr2str(ip.dst)}:{udp.dport}')
+                    dump_eobi(payload, ip.tos, args.love, eth, ip, udp)
                 except (struct.error, eobi.UnpackError) as e:
                     print(f'WARNING: EOBI packet error @{ts}: {e}')
             elif isinstance(tp, dpkt.tcp.TCP):
                 tcp     = tp
                 payload = memoryview(tcp.data)
                 try:
+                    print(f', {addr2str(ip.src)}:{tcp.sport} -> {addr2str(ip.dst)}:{tcp.dport}')
                     dump_eti(payload)
                 except (struct.error, eti.UnpackError) as e:
                     print(f'WARNING: ETI packet error @{ts}: {e}')
+
 
 
 if __name__ == '__main__':
